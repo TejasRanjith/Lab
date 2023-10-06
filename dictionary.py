d14f8633ac13286946ab06618d2cbb2b5663bb5b
Tejas Ranjith <66506433+TejasRanjith@users.noreply.github.com>
2023-10-06 06:04:36 +0000

    added notes.txt and modified dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,11 @@
 thisdict={"brand":"Ford","model":"Mustang","year":1964}
 print(thisdict["brand"])
 thisdict.get("model")
+
+print(thisdict.keys())
+print(thisdict.values())
+#print(thisdict.items())
+thisdict.update({"year":2020})
+print(thisdict)
+thisdict.pop("model")
+print(thisdict)

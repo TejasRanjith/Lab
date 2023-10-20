7719c4bb64d0f22790b3e5dd009464987bb2f86d
Tejas Ranjith <66506433+TejasRanjith@users.noreply.github.com>
2023-10-20 12:27:49 +0530

    for loop

---
+++ python/for_loop.py
@@ -0,0 +1,20 @@
+# for c in "Hello World":
+#     print(c)
+
+# for c in "Hello World":
+#     print(c,end = " ")
+
+# for c in [1,2,3,4]:
+#     print(c,end=" ")
+# for i in range(2,10,2):
+#     print(i,end=" ")
+
+# for (n,s) in [(1,1),(2,4),(3,9),(4,16)]:
+#     print(n," ",s)
+# print()
+
+print()
+
+ages = {"sam":4,"mary":3,"bill":2}
+for name in ages.keys():
+    print(name,ages[name])

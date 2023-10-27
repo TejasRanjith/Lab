6592b16cd211a2ba1b55fea248278eea34646d71
Tejas Ranjith <66506433+TejasRanjith@users.noreply.github.com>
2023-10-27 05:34:45 +0000

    Added Changes

---
+++ DS/exp.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int c;
+    printf("Enter C : ");
+    scanf("%d",&c);
+    printf("%d",c);
+}
--- python/eg_if.py
+++ python/eg_if.py
@@ -1,7 +1,13 @@
-if x ==3:
-    print("X equals 3.")
-elif x == 2:
-    print("X equals 2.")
-else:
-    print("X equals somthing else.")
-print("This is outside the 'if'")
+# if x ==3:
+#     print("X equals 3.")
+# elif x == 2:
+#     print("X equals 2.")
+# else:
+#     print("X equals somthing else.")
+# print("This is outside the 'if'")
+
+# if x>=y:
+#     if x>=z:
+#         print("X is greater.")
+#     else:
+#         print("X is not greater.")

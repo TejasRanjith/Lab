4026a18a5d6bae26edbf6bd89eb61b4afd2504a0
Tejas Ranjith <66506433+TejasRanjith@users.noreply.github.com>
2023-11-29 06:35:51 +0000

    Added Changes

--- DS/bfs-dfs.c
+++ DS/bfs-dfs.c
@@ -12,8 +12,9 @@ void main()
 {
     int n,i,s,ch,j;
     char c,dummy;
-    printf("Enter the number of Vertices : ")
+    printf("Enter the number of Vertices : ");
     scanf("%d",&n);
+    printf("Enter the values of the Matrix Row-Wise : \n");
     for(i=1;i<=n;i++)
     {
         for(j=1;j<=n;j++)
@@ -57,3 +58,126 @@ void main()
     }
     while((c=='y')||(c=='Y'));
 }
+
+
+void bfs(int s,int n)
+{
+    int p,i;
+    add(s);
+    vis[s]=1;
+    p=delete();
+    if(p!=0)
+    {
+        printf(" %d",p);
+    }
+    while(p!=0)
+    {
+        for(i=1;i<=n;i++)
+        {
+            if((a[p][i]!=0)&&(vis[i]==0))
+            {
+                add(i);
+                vis[i]=1;
+            }
+            p=delete();
+            if(p!=0)
+            {
+                printf("%d",p);
+            }
+        }
+        for(i=1;i<=n;i++)
+        {
+            if(vis[i]==0)
+            {
+                bfs(i,n);
+            }
+        }
+    }
+}
+
+void add(int item)
+{
+    if(rear==19)
+    {
+        printf("Queue is full");
+    }
+    else
+    {
+        if(rear==-1)
+        {
+            q[++rear]=item;
+            front++;
+        }
+        else
+        {
+            q[++rear]=item;
+        }
+    }
+}
+
+int delete()
+{
+    int k;
+    if((front>rear)||(front==-1))
+    {
+        return(0);
+    }
+    else
+    {
+        k=q[front++];
+        return(k);
+    }
+}
+
+void dfs(int s,int n)
+{
+    int i,k;
+    push(s);
+    vis[s]=1;
+    k=pop();
+    if(k!=0)
+    {
+        printf(" %d",k);
+    }
+    while(k!=0)
+    {
+        for(i=1;i<=n;i++)
+            if((a[k][i]!=0)&&(vis[i]==0))
+            {
+                push(i);
+                vis[i]=1;
+            }
+            k=pop();
+            if(k!=0)
+            printf(" %d ",k);
+    }
+    for(i=1;i<=n;i++)
+        if(vis[i]==0)
+        dfs(i,n);
+}
+
+void push(int item)
+{
+    if(top==19)
+    {
+        printf("Stack Overflow...");
+    }
+    else
+    {
+        stack[++top]=item;
+    }
+}
+int pop()
+{
+    int k;
+    if(top==-1)
+    {
+        return(0);
+    }
+    else
+    {
+        k=stack[top--];
+        return(k);
+    }
+}
+

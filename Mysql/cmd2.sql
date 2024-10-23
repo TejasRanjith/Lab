7ab426e9de48a9c4d5d0094e09ddb4c60d1dd688
Tejas Ranjith <66506433+TejasRanjith@users.noreply.github.com>
2024-10-23 11:45:56 +0530

    Update cmd2.sql

--- Mysql/cmd2.sql
+++ Mysql/cmd2.sql
@@ -13,3 +13,21 @@ BEGIN
   end if;
 END;
 /
+
+DECLARE
+  -- message varchar2(1000):= 'Hello';
+  A integer(20) := 40;
+  B integer(20) := 20;
+  C integer(20) := 30;
+  Big integer;
+BEGIN 
+  -- dbms_output.put_line(B+A);
+  if (a>b and a>c) then
+    dbms_output.put_line('Biggest of A,B,C is ' || A);
+  elsif (B>C) then
+    dbms_output.put_line('Biggest of A,B,C is ' || B);
+  else
+    dbms_output.put_line('Biggest of A,B,C is ' || C);
+  end if;
+END;
+/

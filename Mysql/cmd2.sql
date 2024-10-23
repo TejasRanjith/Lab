5c2a13b778f2a914c6da7f5ff2690c4a7aed63de
Tejas Ranjith <66506433+TejasRanjith@users.noreply.github.com>
2024-10-23 11:51:59 +0530

    Update cmd2.sql

--- Mysql/cmd2.sql
+++ Mysql/cmd2.sql
@@ -31,3 +31,19 @@ BEGIN
   end if;
 END;
 /
+
+
+DECLARE
+  Grade char(10) := 'B';
+BEGIN 
+  -- dbms_output.put_line(B+A);
+  CASE Grade
+    when 'A' then dbms_output.put_line('Excellent');
+    when 'B' then dbms_output.put_line('Very Good');
+    when 'C' then dbms_output.put_line('Well Done');
+    when 'D' then dbms_output.put_line('You Passed');
+    when 'E' then dbms_output.put_line('Better Luck next Time');
+    else dbms_output.put_line('Invalid Grade');
+  END CASE;
+END;
+/

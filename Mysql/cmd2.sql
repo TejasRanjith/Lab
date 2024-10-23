8c58b577b4d56dd89cec8c8e383b92f690f50d43
Tejas Ranjith <66506433+TejasRanjith@users.noreply.github.com>
2024-10-23 11:39:51 +0530

    Update cmd2.sql

---
+++ Mysql/cmd2.sql
@@ -0,0 +1,15 @@
+DECLARE
+  -- message varchar2(1000):= 'Hello';
+  A integer(20) := 40;
+  B integer(20) := 20;
+  C integer(20) := 30;
+  Big integer;
+BEGIN 
+  -- dbms_output.put_line(B+A);
+  if a > b then 
+    Big:= A;
+  else
+    Big:=B;
+  end if;
+END;
+/
